52fa388b8f85c8a9a5251ea21a8c67f901350b33
Seggy Umboh <seg@seg.gy>
2013-03-14 23:53:55 -0700

    Refactor the form

--- teslatime.js
+++ teslatime.js
@@ -1,12 +1,3 @@
 $(function() {
-  var form;
-  if ($('a[href="/login"]')) {
-    $('<form action="/login" method="post">')
-      .append('<label for="user_session_email">Email</label>')
-      .append('<input name="user_session[email]" id="user_session_email" type="text">')
-      .append('<label for="user_session_password">Password</label>')
-      .append('<input name="user_session[password]" id="user_session_password" type="password">')
-      .append('<input type="submit">')
-      .appendTo(document.body)
-  }
+  $('body').load('/teslatime.html');
 });
